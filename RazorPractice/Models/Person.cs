bba82dd5c4f3f3ec320baad0bb65d85e7767462a
ellisthomas <ellisthomas80@gmail.com>
2017-10-14 10:16:34 -0500

    for 6 complete

---
+++ RazorPractice/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RazorPractice.Models
+{
+    public class Person
+    {
+        public string name;
+        
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+    }
+}
